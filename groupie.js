c1b96ce44147ac28e081e7cb888e672e5d654c4c
Luciano Ropero <lropero@gmail.com>
2023-03-06 16:20:12 -0300

    added tick activity chart

--- groupie.js
+++ groupie.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
+import _ from 'lodash'
+import blessed from 'blessed'
+import cfonts from 'cfonts'
 import chalk from 'chalk'
+import contrib from 'blessed-contrib'
 import figures from 'figures'
 import jsonfile from 'jsonfile'
 import WebSocket from 'ws'
@@ -9,6 +13,40 @@ import { program } from 'commander'
 const BINANCE = 'wss://fstream.binance.com/ws'
 
 const store = {}
+
+const addBox = type => {
+  switch (type) {
+    case 'display': {
+      const { symbol } = store
+      const priceBox = blessed.box({ height: 3, left: symbol.length * 4 + 1, style: { bg: 'black' }, top: 0 })
+      const symbolBox = blessed.box({ height: 3, style: { bg: 'black' }, top: 0 })
+      append({ box: symbolBox, type: 'symbol' })
+      append({ box: priceBox, type: 'price' })
+      break
+    }
+    case 'info': {
+      const { screen } = store
+      const logBox = blessed.box({ style: { bg: 'black' }, top: 2, width: screen.width })
+      append({ box: logBox, type })
+      break
+    }
+    case 'line': {
+      const { screen } = store
+      const lineBox = contrib.line({ height: 20, style: { baseline: 'cyan', bg: 'black', text: 'black' }, top: screen.height - 18, wholeNumbersOnly: true, width: screen.width })
+      append({ box: lineBox, type })
+      break
+    }
+  }
+}
+
+const append = ({ box, type }) => {
+  const { boxes, screen } = store
+  if (boxes[type]) {
+    screen.remove(boxes[type])
+  }
+  screen.append(box)
+  updateStore({ boxes: { ...boxes, [type]: box } })
+}
 
 const boomshakalaka = () => {
   const { currency, id, lastCandle, size, trades } = store
@@ -103,8 +141,23 @@ const createWebSocket = () =>
     webSocket.on('open', () => resolve(webSocket))
   })
 
+const draw = () => {
+  const { boxes, currency, directionColor, lastTrade, lineData, messages, screen, symbol } = store
+  if (lastTrade) {
+    const symbolRender = cfonts.render(symbol, { colors: ['yellow'], font: 'tiny', space: false })
+    boxes.symbol.setContent(symbolRender.string)
+    const priceRender = cfonts.render(currency.format(lastTrade.price), { colors: [directionColor], font: 'tiny', space: false })
+    boxes.price.setContent(priceRender.string)
+  }
+  boxes.info.setContent(messages.map(message => ` ${message}`).join('\n'))
+  if (lineData.x.length > 1) {
+    boxes.line.setData([{ style: { line: 'yellow' }, title: 'Ticks', x: lineData.x, y: lineData.y }])
+  }
+  screen.render()
+}
+
 const log = ({ message, type = '' }) => {
-  console.log(`${logType(type)}${type !== '' ? `${chalk.gray(format(new Date(), 'EEE dd/MM HH:mm:ss'))} ` : ''}${message}`)
+  updateStore({ message: `${logType(type)}${type !== '' ? `${chalk.gray(format(new Date(), 'EEE dd/MM HH:mm:ss'))} ` : ''}${message}` })
 }
 
 const logType = type => {
@@ -136,9 +189,24 @@ const resetWatchdog = () => {
   }, 60000)
 }
 
-const start = () => {
+const start = title => {
+  const { screen } = store
+  addBox('display')
+  addBox('info')
+  addBox('line')
+  screen.key('q', () => process.exit())
+  screen.on(
+    'resize',
+    _.debounce(() => {
+      addBox('display')
+      addBox('info')
+      addBox('line')
+    }, 500)
+  )
+  screen.title = title
   updateStore({ initialized: true })
   connect()
+  setInterval(draw, 50)
 }
 
 const updateStore = updates => {
@@ -148,8 +216,25 @@ const updateStore = updates => {
       store[key] = updates[key]
     } else {
       switch (key) {
+        case 'lastCandle': {
+          const y = store.lineData.y.slice()
+          y.push(updates[key].tickBuy + updates[key].tickSell)
+          while (y.length > 360) {
+            y.shift()
+          }
+          const min = Math.min(...y)
+          store.lineData.y = y.map(value => value - min)
+          store.lineData.x = [...Array(store.lineData.y.length).keys()]
+          store.lastCandle = updates[key]
+          break
+        }
+        case 'message': {
+          const { messages } = store
+          store.messages = [messages[0], updates[key], ...messages.slice(1, 100)]
+          break
+        }
         case 'trade': {
-          const { id, size } = store
+          const { directionColor, id, lastTrade, size } = store
           const { m: marketMaker, p: price, q: quantity, T: tradeTime } = updates[key]
           const trade = { marketMaker, price: parseFloat(price), quantity: parseFloat(quantity), tradeTime }
           const newId = Math.floor(trade.tradeTime / size)
@@ -163,6 +248,7 @@ const updateStore = updates => {
             store.id = newId
             store.trades = []
           }
+          updateStore({ directionColor: trade.price > lastTrade?.price ? 'green' : trade.price < lastTrade?.price ? 'red' : directionColor ?? 'gray', lastTrade: trade })
           store.trades.push(trade)
           break
         }
@@ -187,13 +273,24 @@ program
   .option('-s, --size <seconds>', 'candle size in seconds (defaults to 60)')
   .action(async (symbol, options) => {
     try {
-      const { description, version } = await jsonfile.readFile('./package.json')
-      log({ message: `${chalk.cyan(description.replace('.', ''))} v${version}` })
+      const { description, name, version } = await jsonfile.readFile('./package.json')
       const currency = new Intl.NumberFormat('en-US', { currency: 'USD', minimumFractionDigits: 2, style: 'currency' })
+      const screen = blessed.screen({ forceUnicode: true, fullUnicode: true, smartCSR: true })
       const size = parseInt(options.size ?? 60, 10)
       const webSocket = await createWebSocket()
-      updateStore({ currency, size: size > 0 ? size * 1000 : 60 * 1000, symbol, timers: {}, trades: [], webSocket })
-      start()
+      updateStore({
+        boxes: {},
+        currency,
+        lineData: { x: [], y: [] },
+        messages: [chalk.gray(`${chalk.green(description.replace('.', ''))} v${version} - ${chalk.cyan('q')}uit`)],
+        screen,
+        size: size > 0 ? size * 1000 : 60 * 1000,
+        symbol,
+        timers: {},
+        trades: [],
+        webSocket
+      })
+      start(`${name.charAt(0).toUpperCase()}${name.slice(1)} v${version}`)
     } catch (error) {
       log({ message: error.toString(), type: 'error' })
       process.exit()
